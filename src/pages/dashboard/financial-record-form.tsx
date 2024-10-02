548deaac35d995a34ccce5c0707053f49fb7cdfb
Raskollnikov <bakhalaia56@gmail.com>
2024-10-02 23:02:47 +0400

    for deployment error

--- src/pages/dashboard/financial-record-form.tsx
+++ src/pages/dashboard/financial-record-form.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import { useUser } from "@clerk/clerk-react";
 import { useFinancialRecords } from "../../contexts/financial-record-context";
 
